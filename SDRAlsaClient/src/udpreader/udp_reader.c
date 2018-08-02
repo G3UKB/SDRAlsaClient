43ce218b4a9d1df1097de1ff9871d557a879188e
Bob Cowdery <bob@bobcowdery.plus.com>
2018-08-02 09:39:08 +0100

    Developing.

---
+++ SDRAlsaClient/src/udpreader/udp_reader.c
@@ -0,0 +1,66 @@
+/*
+udp_reader.c
+
+Read data from UDP.
+
+Copyright (C) 2018 by G3UKB Bob Cowdery
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+The authors can be reached by email at:
+
+	bob@bobcowdery.plus.com
+*/
+
+// Includes
+#include "../common/include.h"
+
+// Forward refs
+static void udprecvdata(int sd, struct sockaddr_in *cliAddr);
+
+// Module vars
+unsigned char pcdata[METIS_FRAME_SZ];
+
+// Thread entry point for ALSA processing
+void udp_reader_imp(void* data){
+    // Get our thread parameters
+    udp_thread_data* td = (udp_thread_data*)data;
+    int sd = td->socket;
+    struct sockaddr_in *cli_addr = td->cli_addr;
+
+    printf("Started UDP reader thread\n");
+
+    while (td->terminate == FALSE) {
+         udprecvdata(sd, cli_addr);
+         sleep(0.1);
+    }
+
+    printf("UDP Reader thread exiting...\n");
+}
+
+
+// Read and discard data but extract the tuning frequency
+// Set the FCD to the frequency
+// ToDo - Enhance for other sound card type devices
+static void udprecvdata(int sd, struct sockaddr_in *cliAddr) {
+
+    unsigned int addr_sz = sizeof(*cliAddr);
+
+    // Read a frame size data packet
+    n = recvfrom(sd, pcdata, METIS_FRAME_SZ, 0, (struct sockaddr_in *)cliAddr, &addr_sz);
+    if(n == METIS_FRAME_SZ) {
+
+    }
+}
