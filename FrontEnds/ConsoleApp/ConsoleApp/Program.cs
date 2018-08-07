915e1366dcc9392c5aa3b1d99b21273c5ba4b29f
Bob Cowdery <bob@bobcowdery.plus.com>
2018-08-07 17:59:52 +0100

    Developing.

---
+++ FrontEnds/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace SDRAlsaConsole
+{
+    class Program
+    {
+        [DllImport("E:\\Projects\\SDRAlsaClient\\trunk\\SDRAlsaClient\\bin\\Debug\\SDRAlsaClient.dll")]
+        private static extern int RunClient();
+        [DllImport("E:\\Projects\\SDRAlsaClient\\trunk\\SDRAlsaClient\\bin\\Debug\\SDRAlsaClient.dll")]
+        private static extern IntPtr AppLastError();
+
+        static void Main(string[] args)
+        {
+            int r = RunClient();
+            if (r==-1)
+            {
+                string s = Marshal.PtrToStringAnsi(AppLastError());
+                Console.WriteLine("Error: {0}", s);
+            }
+            Console.WriteLine("Exiting...");
+        }
+    }
+}
